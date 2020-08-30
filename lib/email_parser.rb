69c515dff10a3e30702044ffe91033ca1bc3ed33
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-30 20:35:26 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,7 +4,9 @@
 # or whitespace (' ').
 class EmailAddressParser
   
-  attr_accessor :emails
+  attr_accessor :emails, :name
+  
+  def initialize(name, emails)
   
   def parse
     emails.split.collect do |address|
